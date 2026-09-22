794670d87cd63d38deebafd5ba1bb234a9bc5fd6
laran <quintanarijoan@gmail.com>
2026-09-21 21:20:04 -0300

    superv: control de stock lista productos y solo deja modificar stock

--- KioscoApp/UcAuditoriaCajas.Designer.cs
+++ KioscoApp/UcAuditoriaCajas.Designer.cs
@@ -36,185 +36,185 @@
             // lblTitulo
             // 
             lblTitulo.AutoSize = true;
-            lblTitulo.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            lblTitulo.Location = new System.Drawing.Point(30, 20);
-            lblTitulo.Name = "lblTitulo";
-            lblTitulo.Size = new System.Drawing.Size(315, 30);
-            lblTitulo.TabIndex = 0;
             lblTitulo.BackColor = SystemColors.ControlDarkDark;
-            lblTitulo.Text = "Auditor�a y Control de Cajas";
+            lblTitulo.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            lblTitulo.Location = new Point(30, 20);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(303, 30);
+            lblTitulo.TabIndex = 0;
+            lblTitulo.Text = "Auditoría y Control de Cajas";
             // 
             // gbControles
             // 
             gbControles.Controls.Add(btnRetiro);
             gbControles.Controls.Add(btnArqueo);
             gbControles.Controls.Add(btnHistorial);
-            gbControles.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            gbControles.Location = new System.Drawing.Point(30, 80);
+            gbControles.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            gbControles.ForeColor = SystemColors.ControlLightLight;
+            gbControles.Location = new Point(30, 80);
             gbControles.Name = "gbControles";
-            gbControles.Size = new System.Drawing.Size(280, 250);
+            gbControles.Size = new Size(280, 250);
             gbControles.TabIndex = 1;
             gbControles.TabStop = false;
-            gbControles.ForeColor = SystemColors.ControlLightLight;
             gbControles.Text = "Controles Diarios";
-            // 
-            // btnHistorial
-            // 
-            btnHistorial.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            btnHistorial.Location = new System.Drawing.Point(20, 40);
-            btnHistorial.Name = "btnHistorial";
-            btnHistorial.Size = new System.Drawing.Size(240, 50);
-            btnHistorial.TabIndex = 0;
-            btnHistorial.Text = "Historial de Cierres (Z)";
-            btnHistorial.BackColor = SystemColors.AppWorkspace;
-            btnHistorial.ForeColor = SystemColors.ButtonHighlight;
-            btnHistorial.UseVisualStyleBackColor = false;
-            btnHistorial.Click += MostrarProximamente;
-            // 
-            // btnArqueo
-            // 
-            btnArqueo.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            btnArqueo.Location = new System.Drawing.Point(20, 110);
-            btnArqueo.Name = "btnArqueo";
-            btnArqueo.Size = new System.Drawing.Size(240, 50);
-            btnArqueo.TabIndex = 1;
-            btnArqueo.Text = "Arqueo Sorpresa (Corte X)";
-            btnArqueo.BackColor = SystemColors.AppWorkspace;
-            btnArqueo.ForeColor = SystemColors.ButtonHighlight;
-            btnArqueo.UseVisualStyleBackColor = false;
-            btnArqueo.Click += MostrarProximamente;
             // 
             // btnRetiro
             // 
-            btnRetiro.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            btnRetiro.Location = new System.Drawing.Point(20, 180);
+            btnRetiro.BackColor = SystemColors.AppWorkspace;
+            btnRetiro.Font = new Font("Segoe UI", 10F);
+            btnRetiro.ForeColor = SystemColors.ButtonHighlight;
+            btnRetiro.Location = new Point(20, 180);
             btnRetiro.Name = "btnRetiro";
-            btnRetiro.Size = new System.Drawing.Size(240, 50);
+            btnRetiro.Size = new Size(240, 50);
             btnRetiro.TabIndex = 2;
             btnRetiro.Text = "Registrar Gasto / Retiro";
-            btnRetiro.BackColor = SystemColors.AppWorkspace;
-            btnRetiro.ForeColor = SystemColors.ButtonHighlight;
             btnRetiro.UseVisualStyleBackColor = false;
             btnRetiro.Click += MostrarProximamente;
+            // 
+            // btnArqueo
+            // 
+            btnArqueo.BackColor = SystemColors.AppWorkspace;
+            btnArqueo.Font = new Font("Segoe UI", 10F);
+            btnArqueo.ForeColor = SystemColors.ButtonHighlight;
+            btnArqueo.Location = new Point(20, 110);
+            btnArqueo.Name = "btnArqueo";
+            btnArqueo.Size = new Size(240, 50);
+            btnArqueo.TabIndex = 1;
+            btnArqueo.Text = "Arqueo Sorpresa (Corte X)";
+            btnArqueo.UseVisualStyleBackColor = false;
+            btnArqueo.Click += MostrarProximamente;
+            // 
+            // btnHistorial
+            // 
+            btnHistorial.BackColor = SystemColors.AppWorkspace;
+            btnHistorial.Font = new Font("Segoe UI", 10F);
+            btnHistorial.ForeColor = SystemColors.ButtonHighlight;
+            btnHistorial.Location = new Point(20, 40);
+            btnHistorial.Name = "btnHistorial";
+            btnHistorial.Size = new Size(240, 50);
+            btnHistorial.TabIndex = 0;
+            btnHistorial.Text = "Historial de Cierres (Z)";
+            btnHistorial.UseVisualStyleBackColor = false;
+            btnHistorial.Click += MostrarProximamente;
             // 
             // gbReportes
             // 
             gbReportes.Controls.Add(btnAnular);
             gbReportes.Controls.Add(btnHorarios);
             gbReportes.Controls.Add(btnFaltantes);
-            gbReportes.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            gbReportes.Location = new System.Drawing.Point(340, 80);
+            gbReportes.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            gbReportes.ForeColor = SystemColors.ControlLightLight;
+            gbReportes.Location = new Point(340, 80);
             gbReportes.Name = "gbReportes";
-            gbReportes.Size = new System.Drawing.Size(280, 250);
+            gbReportes.Size = new Size(280, 250);
             gbReportes.TabIndex = 2;
             gbReportes.TabStop = false;
-            gbReportes.ForeColor = SystemColors.ControlLightLight;
             gbReportes.Text = "Análisis y Rendimiento";
-            // 
-            // btnFaltantes
-            // 
-            btnFaltantes.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            btnFaltantes.Location = new System.Drawing.Point(20, 40);
-            btnFaltantes.Name = "btnFaltantes";
-            btnFaltantes.Size = new System.Drawing.Size(240, 50);
-            btnFaltantes.TabIndex = 0;
-            btnFaltantes.Text = "Descuadres por Empleado";
-            btnFaltantes.BackColor = SystemColors.AppWorkspace;
-            btnFaltantes.ForeColor = SystemColors.ButtonHighlight;
-            btnFaltantes.UseVisualStyleBackColor = false;
-            btnFaltantes.Click += MostrarProximamente;
-            // 
-            // btnHorarios
-            // 
-            btnHorarios.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            btnHorarios.Location = new System.Drawing.Point(20, 110);
-            btnHorarios.Name = "btnHorarios";
-            btnHorarios.Size = new System.Drawing.Size(240, 50);
-            btnHorarios.TabIndex = 1;
-            btnHorarios.Text = "Ranking de Ventas por Hora";
-            btnHorarios.BackColor = SystemColors.AppWorkspace;
-            btnHorarios.ForeColor = SystemColors.ButtonHighlight;
-            btnHorarios.UseVisualStyleBackColor = false;
-            btnHorarios.Click += MostrarProximamente;
             // 
             // btnAnular
             // 
-            btnAnular.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            btnAnular.Location = new System.Drawing.Point(20, 180);
-            btnAnular.Name = "btnAnular";
-            btnAnular.Size = new System.Drawing.Size(240, 50);
-            btnAnular.TabIndex = 2;
-            btnAnular.Text = "Auditor�a de Ventas Anuladas";
             btnAnular.BackColor = SystemColors.AppWorkspace;
+            btnAnular.Font = new Font("Segoe UI", 10F);
             btnAnular.ForeColor = SystemColors.ButtonHighlight;
+            btnAnular.Location = new Point(20, 180);
+            btnAnular.Name = "btnAnular";
+            btnAnular.Size = new Size(240, 50);
+            btnAnular.TabIndex = 2;
+            btnAnular.Text = "Auditoría de Ventas Anuladas";
             btnAnular.UseVisualStyleBackColor = false;
             btnAnular.Click += MostrarProximamente;
+            // 
+            // btnHorarios
+            // 
+            btnHorarios.BackColor = SystemColors.AppWorkspace;
+            btnHorarios.Font = new Font("Segoe UI", 10F);
+            btnHorarios.ForeColor = SystemColors.ButtonHighlight;
+            btnHorarios.Location = new Point(20, 110);
+            btnHorarios.Name = "btnHorarios";
+            btnHorarios.Size = new Size(240, 50);
+            btnHorarios.TabIndex = 1;
+            btnHorarios.Text = "Ranking de Ventas por Hora";
+            btnHorarios.UseVisualStyleBackColor = false;
+            btnHorarios.Click += MostrarProximamente;
+            // 
+            // btnFaltantes
+            // 
+            btnFaltantes.BackColor = SystemColors.AppWorkspace;
+            btnFaltantes.Font = new Font("Segoe UI", 10F);
+            btnFaltantes.ForeColor = SystemColors.ButtonHighlight;
+            btnFaltantes.Location = new Point(20, 40);
+            btnFaltantes.Name = "btnFaltantes";
+            btnFaltantes.Size = new Size(240, 50);
+            btnFaltantes.TabIndex = 0;
+            btnFaltantes.Text = "Descuadres por Empleado";
+            btnFaltantes.UseVisualStyleBackColor = false;
+            btnFaltantes.Click += MostrarProximamente;
             // 
             // gbHerramientas
             // 
             gbHerramientas.Controls.Add(btnInventario);
             gbHerramientas.Controls.Add(btnExportarCSV);
             gbHerramientas.Controls.Add(btnImportarExcel);
-            gbHerramientas.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            gbHerramientas.Location = new System.Drawing.Point(650, 80);
+            gbHerramientas.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            gbHerramientas.ForeColor = SystemColors.ControlLightLight;
+            gbHerramientas.Location = new Point(650, 80);
             gbHerramientas.Name = "gbHerramientas";
-            gbHerramientas.Size = new System.Drawing.Size(280, 250);
+            gbHerramientas.Size = new Size(280, 250);
             gbHerramientas.TabIndex = 3;
             gbHerramientas.TabStop = false;
-            gbHerramientas.ForeColor = SystemColors.ControlLightLight;
             gbHerramientas.Text = "Herramientas de Supervisor";
-            // 
-            // btnImportarExcel
-            // 
-            btnImportarExcel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            btnImportarExcel.Location = new System.Drawing.Point(20, 40);
-            btnImportarExcel.Name = "btnImportarExcel";
-            btnImportarExcel.Size = new System.Drawing.Size(240, 50);
-            btnImportarExcel.TabIndex = 0;
-            btnImportarExcel.Text = "Actualizar Precios (Excel)";
-            btnImportarExcel.BackColor = SystemColors.AppWorkspace;
-            btnImportarExcel.ForeColor = SystemColors.ButtonHighlight;
-            btnImportarExcel.UseVisualStyleBackColor = false;
-            btnImportarExcel.Click += MostrarProximamente;
-            // 
-            // btnExportarCSV
-            // 
-            btnExportarCSV.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            btnExportarCSV.Location = new System.Drawing.Point(20, 110);
-            btnExportarCSV.Name = "btnExportarCSV";
-            btnExportarCSV.Size = new System.Drawing.Size(240, 50);
-            btnExportarCSV.TabIndex = 1;
-            btnExportarCSV.Text = "Exportar Datos Contador (CSV)";
-            btnExportarCSV.BackColor = SystemColors.AppWorkspace;
-            btnExportarCSV.ForeColor = SystemColors.ButtonHighlight;
-            btnExportarCSV.UseVisualStyleBackColor = false;
-            btnExportarCSV.Click += MostrarProximamente;
             // 
             // btnInventario
             // 
-            btnInventario.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            btnInventario.Location = new System.Drawing.Point(20, 180);
+            btnInventario.BackColor = SystemColors.AppWorkspace;
+            btnInventario.Font = new Font("Segoe UI", 10F);
+            btnInventario.ForeColor = SystemColors.ButtonHighlight;
+            btnInventario.Location = new Point(20, 180);
             btnInventario.Name = "btnInventario";
-            btnInventario.Size = new System.Drawing.Size(240, 50);
+            btnInventario.Size = new Size(240, 50);
             btnInventario.TabIndex = 2;
             btnInventario.Text = "Reporte Inventario Valorizado";
-            btnInventario.BackColor = SystemColors.AppWorkspace;
-            btnInventario.ForeColor = SystemColors.ButtonHighlight;
             btnInventario.UseVisualStyleBackColor = false;
             btnInventario.Click += MostrarProximamente;
             // 
+            // btnExportarCSV
+            // 
+            btnExportarCSV.BackColor = SystemColors.AppWorkspace;
+            btnExportarCSV.Font = new Font("Segoe UI", 10F);
+            btnExportarCSV.ForeColor = SystemColors.ButtonHighlight;
+            btnExportarCSV.Location = new Point(20, 110);
+            btnExportarCSV.Name = "btnExportarCSV";
+            btnExportarCSV.Size = new Size(240, 50);
+            btnExportarCSV.TabIndex = 1;
+            btnExportarCSV.Text = "Exportar Datos Contador (CSV)";
+            btnExportarCSV.UseVisualStyleBackColor = false;
+            btnExportarCSV.Click += MostrarProximamente;
+            // 
+            // btnImportarExcel
+            // 
+            btnImportarExcel.BackColor = SystemColors.AppWorkspace;
+            btnImportarExcel.Font = new Font("Segoe UI", 10F);
+            btnImportarExcel.ForeColor = SystemColors.ButtonHighlight;
+            btnImportarExcel.Location = new Point(20, 40);
+            btnImportarExcel.Name = "btnImportarExcel";
+            btnImportarExcel.Size = new Size(240, 50);
+            btnImportarExcel.TabIndex = 0;
+            btnImportarExcel.Text = "Actualizar Precios (Excel)";
+            btnImportarExcel.UseVisualStyleBackColor = false;
+            btnImportarExcel.Click += MostrarProximamente;
+            // 
             // UcAuditoriaCajas
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ControlDarkDark;
-            ForeColor = SystemColors.ControlLightLight;
             Controls.Add(gbHerramientas);
             Controls.Add(gbReportes);
             Controls.Add(gbControles);
             Controls.Add(lblTitulo);
+            ForeColor = SystemColors.ControlLightLight;
             Name = "UcAuditoriaCajas";
-            Size = new System.Drawing.Size(1000, 643);
+            Size = new Size(1000, 643);
             gbControles.ResumeLayout(false);
             gbReportes.ResumeLayout(false);
             gbHerramientas.ResumeLayout(false);
--- KioscoApp/UcMovimientosStock.cs
+++ KioscoApp/UcMovimientosStock.cs
@@ -1,15 +1,120 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace KioscoApp
 {
     public partial class UcMovimientosStock : UserControl
     {
+        private string connectionString = "Server=localhost\\SQLEXPRESS;Database=KioscoDB;Integrated Security=True;";
+        private int idProductoSeleccionado = -1;
+
         public UcMovimientosStock()
         {
             InitializeComponent();
-            
+            ConfigurarGrilla();
+            CargarStock();
+        }
+
+        private void ConfigurarGrilla()
+        {
+            dgvStock.AutoGenerateColumns = false;
+            dgvStock.Columns.Clear();
+
+            dgvStock.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id", Visible = false });
+            dgvStock.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "SKU", HeaderText = "SKU (Código)", Width = 150 });
+            dgvStock.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Nombre", HeaderText = "Producto", Width = 250 });
+            dgvStock.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Categoria", HeaderText = "Categoría", Width = 150 });
+            dgvStock.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "StockActual", HeaderText = "Stock Actual", Width = 100 });
+            dgvStock.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "StockMinimo", HeaderText = "Stock Mínimo", Width = 100 });
+        }
+
+        private void CargarStock(string filtro = "")
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    string query = @"
+                        SELECT p.Id, p.SKU, p.Nombre, c.Nombre AS Categoria, p.StockActual, p.StockMinimo 
+                        FROM Productos p
+                        LEFT JOIN Categorias c ON p.IdCategoria = c.Id
+                        WHERE p.Nombre LIKE @Filtro OR p.SKU LIKE @Filtro
+                        ORDER BY p.Nombre";
+                    
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Filtro", "%" + filtro + "%");
+                        using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                        {
+                            DataTable dt = new DataTable();
+                            da.Fill(dt);
+                            dgvStock.DataSource = dt;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar el stock: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            CargarStock(txtBuscar.Text);
+        }
+
+        private void dgvStock_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvStock.CurrentRow != null)
+            {
+                idProductoSeleccionado = Convert.ToInt32(dgvStock.CurrentRow.Cells[0].Value);
+                lblProductoSeleccionado.Text = dgvStock.CurrentRow.Cells[2].Value.ToString();
+                numStockActual.Value = Convert.ToDecimal(dgvStock.CurrentRow.Cells[4].Value);
+                numStockMinimo.Value = Convert.ToDecimal(dgvStock.CurrentRow.Cells[5].Value);
+            }
+            else
+            {
+                idProductoSeleccionado = -1;
+                lblProductoSeleccionado.Text = "Ningún producto seleccionado";
+                numStockActual.Value = 0;
+                numStockMinimo.Value = 0;
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (idProductoSeleccionado == -1)
+            {
+                MessageBox.Show("Seleccione un producto de la grilla para modificar su stock.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    string query = "UPDATE Productos SET StockActual = @Actual, StockMinimo = @Minimo WHERE Id = @Id";
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Actual", (int)numStockActual.Value);
+                        cmd.Parameters.AddWithValue("@Minimo", (int)numStockMinimo.Value);
+                        cmd.Parameters.AddWithValue("@Id", idProductoSeleccionado);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show("Stock actualizado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                CargarStock(txtBuscar.Text); // Recargar
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar el stock: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
-
